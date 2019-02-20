1ab30402d93a8969e8a090ea9e7a16db8d23ddd0
Jenny Li <wangjunqian@wangjunqiandeMacBook-Pro-2.local>
2019-02-20 14:09:40 -0500

    Created Flix app for CodePath assignment 1

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Jenny Li on 2/20/19.
+//  Copyright © 2019 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
