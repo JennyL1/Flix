3d966001cf97dc84562d342e933e9bfad11dd093
Jenny Li <wangjunqian@wangjunqiandeMacBook-Pro-2.local>
2019-02-27 18:43:03 -0500

    Added Flix pt 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jenny Li on 2/27/19.
+//  Copyright © 2019 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
